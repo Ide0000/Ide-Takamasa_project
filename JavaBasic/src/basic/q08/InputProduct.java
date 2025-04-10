b60bc322857f17160e95f07bf4e228cb800eaa5e
takamasa <takamasa@localhost>
2025-04-10 23:11:48 +0900

    ３週目課題

---
+++ JavaBasic/src/basic/q08/InputProduct.java
@@ -0,0 +1,16 @@
+package basic.q08;
+
+import java.util.Scanner;
+
+public class InputProduct {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("商品名を入力してください：");
+		String productname = scanner.nextLine();
+		System.out.println("価格を入力してください：");
+		int price = scanner.nextInt();
+		System.out.println("商品名は" + productname + "です。価格は" + price + "円です。");
+		scanner.close();
+	}
+
+}
